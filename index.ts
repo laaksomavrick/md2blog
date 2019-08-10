ffd346355c7cff45976034c87f07d11cd90331b1
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2019-08-10 15:35:37 -0400

    v0.2.0

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
+#!/usr/bin/env node
+
 import { buildCli } from "./lib/cli";
 import config from "./lib/config";
 
 function main(): void {
     try {
         const cli = buildCli();
+
         cli.parse(process.argv);
+
+        if (cli.args.length === 0) {
+            cli.help();
+        }
+
         process.exit(0);
     } catch (e) {
         if (config.get("env") === "development") {
